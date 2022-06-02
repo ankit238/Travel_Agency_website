6616f32cafbcf7fbdefaaba8097ac7bfe11b1f5d
Ankit Kumar <79011661+ankit238@users.noreply.github.com>
2022-06-02 22:33:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import scrollreveal from 'scrollreveal';
 import './App.css';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Recommend from './components/Recommend';
+import ScroolToTop from './components/ScroolToTop';
+import Services from './components/Services';
+import Testimonial from './components/Testimonial';
 
 function App() {
+
+  useEffect(() => {
+    const sr = scrollreveal({
+      origin: "top",
+      distance: "80px",
+      duration: 2000,
+      reset: true,
+    });
+    sr.reveal(
+      `
+    nav,
+    #hero,
+    #recommend,
+    #testimonials,
+    footer
+    `, {
+      opacity: 0,
+      interval: 300,
+    }
+    );
+  }, []);
+
+ 
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <Navbar />
+      <Hero />
+      <Services />
+      <Recommend />
+      <Testimonial />
+      <Footer />
+      <ScroolToTop />
+    </>
   );
 }
 
